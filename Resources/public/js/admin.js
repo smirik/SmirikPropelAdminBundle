15e20ada4ba1527a3103b586003365e80d139311
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2013-03-22 12:34:22 +0400

    Fix form sortable & add onsubmit event support

--- Resources/public/js/admin.js
+++ Resources/public/js/admin.js
@@ -1,5 +1,5 @@
-$(document).on('ready', function(e){
-    
+$(document).on('ready', function (e) {
+
     $(document).on("click", ".sortable", function (e) {
         e.preventDefault();
         linkToContent(this);
@@ -16,7 +16,7 @@ $(document).on('ready', function(e){
             linkToContent(this);
         }
     });
-    
+
     $('.alert-place').ajaxError(function (event, request, settings) {
         $('.progress').hide();
         var alert;
@@ -38,59 +38,83 @@ $(document).on('ready', function(e){
         $('<span/>').html(text).appendTo(body);
         return body;
     }
+    $(document).on('click', 'a.ajax_flag', function (e) {
+            e.preventDefault();
+            /* @todo multiple choices */
+            /* @todo refactoring */
+            var that = $(this),
+                data = that.data(),
+                status = (data.status == 1) ? 0 : 1;
+
+            if (that.attr('disabled')) {
+                return false;
+            }
+
+            that.attr('disabled', true);
+            $.post(that.attr('href'), { 'status': status, 'id': data.id }, function (response) {
+                that.attr('disabled', false);
+                that.data('status', status);
+                $.each(data.text, function (e) {
+                    if (this.key == status) {
+                        that.html(this.text);
+                    }
+                });
+
+            }, 'json');
+        });
 });
 
 var loadContent = function (path) {
     $('.progress').show();
     $.ajaxSetup({
-        cache:false
+        cache: false
     });
+
     $.get(path, $('form.filterable').serialize(), function (html) {
         $('.progress').hide();
         $('#content').html(html);
         $('.ajax_action').tomodal();
     }, 'html');
 }
+$(document).on('submit', 'form.filterable', function (e) {
+    e.preventDefault();
+    loadContent($(this).attr('action'));
+});
 
 var linkToContent = function (el) {
     var link = $(el).attr('href') ? $(el).attr('href') : $(el).data('href');
     if (!el) return false;
-    loadContent(el);
+    loadContent(link);
 };
 
 
-function assign_mass_action(route, alert_text, flush)
-{
-	var checked = [];
-	$.each($('.admin_item_checkbox'), function(index, value){
-		if (value.checked)
-		{
-			checked.push(value.value);
-		}
-	});
-	var obj = {
-		ids: checked
-	}
-	$.post(route, $.param(obj), function(data){
-		if (flush)
-		{
-			$.each($('.admin_item_checkbox'), function(index, value){
-				value.checked = false;
-			});
-		}
-		console.log(data);
-		$('#alerts').append($('#alert_template').clone().show());
-	});
+function assign_mass_action(route, alert_text, flush) {
+    var checked = [];
+    $.each($('.admin_item_checkbox'), function (index, value) {
+        if (value.checked) {
+            checked.push(value.value);
+        }
+    });
+    var obj = {
+        ids: checked
+    }
+    $.post(route, $.param(obj), function (data) {
+        if (flush) {
+            $.each($('.admin_item_checkbox'), function (index, value) {
+                value.checked = false;
+            });
+        }
+        console.log(data);
+        $('#alerts').append($('#alert_template').clone().show());
+    });
 }
 
-function checkbox_mass_action(ids)
-{
-	var obj = $.parseJSON(ids);
-	$.each($('.admin_item_checkbox'), function(index, value){
-		console.log(value.value.toString());
-		if ($.inArray(value.value.toString(), obj) > -1)
-		{
-			value.checked = true;
-		}
-	});
+function checkbox_mass_action(ids) {
+    var obj = $.parseJSON(ids);
+    $.each($('.admin_item_checkbox'), function (index, value) {
+        console.log(value.value.toString());
+        if ($.inArray(value.value.toString(), obj) > -1) {
+            value.checked = true;
+        }
+    });
 }
